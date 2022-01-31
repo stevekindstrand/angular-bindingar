6270aff278f22faf8e7cafd66d9764462209d16f
stevekindstrand <stevekindstrand@gmail.com>
2022-01-31 11:01:12 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NONE_TYPE } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bindingar';
+
+  imageUrl: string = 'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074_960_720.jpg';
+  imageUrlClass: string = 'imageUrlClass';
+
+  divClass: string = 'divClass';
+
+  boolTest: boolean = true;
+
+  button = () => {
+    document.body.style.background = 'black';
+  }
 }
